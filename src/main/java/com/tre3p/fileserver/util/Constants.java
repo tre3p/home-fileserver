ef01d98d631a1747db53a6229328487fafb3420b
tre3p <whereareyo@yahoo.com>
2022-06-09 21:42:38 +0300

    add zip archive encryption

---
+++ src/main/java/com/tre3p/fileserver/util/Constants.java
@@ -0,0 +1,7 @@
+package com.tre3p.fileserver.util;
+
+public class Constants {
+    public static final String DATASTORAGE = "/application/datastorage/";
+    public static final String ZIP = ".zip";
+    public static final String ENCRYPTION_ALGORITHM = "AES";
+}
