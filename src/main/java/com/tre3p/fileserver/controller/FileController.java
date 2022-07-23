8dbe5847e18f67fbecc25ac08b9017819dbfb28a
tr33p <whereareyo@yahoo.com>
2022-07-23 21:32:26 +0300

    CORS added

--- src/main/java/com/tre3p/fileserver/controller/FileController.java
+++ src/main/java/com/tre3p/fileserver/controller/FileController.java
@@ -3,7 +3,14 @@ package com.tre3p.fileserver.controller;
 import com.tre3p.fileserver.model.FileMetadata;
 import com.tre3p.fileserver.service.FileService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.crypto.BadPaddingException;
