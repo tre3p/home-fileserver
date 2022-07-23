ce7e612133f74355f8a49ca9cfe4306188c35205
tr33p <whereareyo@yahoo.com>
2022-07-23 13:35:44 +0300

    frontend test

--- src/main/java/com/tre3p/fileserver/controller/FileController.java
+++ src/main/java/com/tre3p/fileserver/controller/FileController.java
@@ -1,11 +1,15 @@
 package com.tre3p.fileserver.controller;
 
 import com.tre3p.fileserver.model.FileMetadata;
-import com.tre3p.fileserver.repository.FileRepository;
 import com.tre3p.fileserver.service.FileService;
-import liquibase.pro.packaged.F;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.crypto.BadPaddingException;
@@ -32,7 +36,12 @@ public class FileController {
     }
 
     @PostMapping
-    public void uploadFile(@RequestPart("data") MultipartFile file) throws IOException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
+    public void uploadFile(@RequestPart("data") MultipartFile file) throws IOException,
+            NoSuchPaddingException,
+            IllegalBlockSizeException,
+            NoSuchAlgorithmException,
+            BadPaddingException,
+            InvalidKeyException {
         File file1 = new File("tmp");
         FileOutputStream fos = new FileOutputStream(file1);
         fos.write(file.getBytes());
