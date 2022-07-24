4606ab3d8018b5a5c599ca10b649ad4129408211
tr33p <whereareyo@yahoo.com>
2022-07-24 23:57:28 +0300

    lint fixes

--- src/main/java/com/tre3p/fileserver/security/SecurityConfig.java
+++ src/main/java/com/tre3p/fileserver/security/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.tre3p.fileserver.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
