161422d6d4e933b9cd6ac7dff49142e96ee758e5
tr33p <whereareyo@yahoo.com>
2022-07-24 23:54:47 +0300

    fix security to allow file upload

--- src/main/java/com/tre3p/fileserver/security/SecurityConfig.java
+++ src/main/java/com/tre3p/fileserver/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.tre3p.fileserver.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,18 +14,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/*").permitAll();
-    }
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/*");
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/").permitAll()
+                .antMatchers(HttpMethod.POST, "/").permitAll()
+                .and()
+                .csrf().disable()
+                .httpBasic().disable()
+                .sessionManagement().disable()
+                .logout().disable()
+                .requestCache().disable();
     }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("*")
                 .allowedMethods("*");
     }
 }
