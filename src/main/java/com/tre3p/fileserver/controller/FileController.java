f68edc0b5baa9028d679ecdb098ec2918244e8ad
tr33p <whereareyo@yahoo.com>
2022-07-24 17:19:49 +0300

    docker-fix

--- src/main/java/com/tre3p/fileserver/controller/FileController.java
+++ src/main/java/com/tre3p/fileserver/controller/FileController.java
@@ -3,7 +3,6 @@ package com.tre3p.fileserver.controller;
 import com.tre3p.fileserver.model.FileMetadata;
 import com.tre3p.fileserver.service.FileService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
